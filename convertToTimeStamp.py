2a6d9ee7544e326fc09dd9224ada7a2c8b8baa31
Juan Pedro <JuanP.95.torres@gmail.com>
2017-12-16 02:02:45 +0100

    Refactor

--- convertToTimeStamp.py
+++ convertToTimeStamp.py
@@ -1,12 +1,11 @@
 
 def keyToTimeStamp(key):
-	PUSH_CHARS = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'
+        PUSH_CHARS ='-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'
+        idString = key[0:8]
+        time = 0
+        for letter in idString:
+                time = time * 64 + PUSH_CHARS.index(letter)
 
-	s1 = key[0:8]
+        return time
 
-	time = 0
-
-	for letter in s1:
-		time = time * 64 + PUSH_CHARS.index(letter)
-
-	return time 
+print (keyToTimeStamp("-L0RhEU-MG5lfV35Ut_c"))
